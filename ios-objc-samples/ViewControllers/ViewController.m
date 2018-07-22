1c88748d51b906947478a24a1a61e07c65821c44
OkuderaYuki <>
2018-07-22 16:08:38 +0900

    Display the text with UILabel.

--- ios-objc-samples/ViewControllers/ViewController.m
+++ ios-objc-samples/ViewControllers/ViewController.m
@@ -9,14 +9,30 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *oneLineLabel;
+@property (weak, nonatomic) IBOutlet UILabel *twoLinesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *anyLinesLabel;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Life cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self outputText];
+}
+
+#pragma mark - Others
+
+/**
+ ラベルに文字列を設定する
+ */
+- (void)outputText {
+    NSString *const sampleText = @"UILabelのサンプルです。\nStoryboardでUILabelのLinesに0を設定すると、\n複数行の文字列を表示することができます。";
+    self.anyLinesLabel.text = sampleText;
 }
 
 
