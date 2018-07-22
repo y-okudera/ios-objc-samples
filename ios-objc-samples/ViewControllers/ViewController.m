61cc7783ab12adf7375eb607540b9ab725a419ef
OkuderaYuki <>
2018-07-22 22:30:27 +0900

    Add sample of UIButton.

--- ios-objc-samples/ViewControllers/ViewController.m
+++ ios-objc-samples/ViewControllers/ViewController.m
@@ -18,6 +18,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)didTapOutputLogButton:(UIButton *)sender {
+    NSLog(@"(%s Line: %d) I tapped the button!", __func__, __LINE__);
+}
 
+// ボタンのsuperviewがuserInteractionEnabled = NOのため、ボタンのタップイベントは検出されない
+- (IBAction)didTapButtonOnTheView:(UIButton *)sender {
+    NSLog(@"I tapped button. The button's super view is disabled user interaction.");
+}
 
 @end
